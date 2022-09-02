f8c00bfb45b71c7196b8bcdbf1eba1d8b3d29bac
Sergey Ugryumov <56644580+Reventaine@users.noreply.github.com>
2022-09-02 18:43:31 +0300

    Update config.py

--- sections/config.py
+++ sections/config.py
@@ -1,8 +1,8 @@
 # postgres db info:
-dbname = 'TeleShop'
-user = 'postgres'
-password = '2480353'
+dbname = 'DB NAME'
+user = 'USER'
+password = 'PASS'
 host = 'localhost'
 
 # telegram bot token:
-teleToken = "5733351271:AAFw5EzbJo-a4cZGzi0F_yocbnxm1_tngMM"
+teleToken = "TELEGRAM TOKEN"
